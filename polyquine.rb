484d1417f9b281428ab1c116e4695f5a6d658ad6
Abhishek Keshri <keshri.abhishek63@gmail.com>
2020-12-31 03:30:48 +0530

    Add Polyquine in Ruby

---
+++ polyquine.rb
@@ -0,0 +1,10 @@
+ #include/*
+s='''*/<stdio.h>
+main(){char*_;/*==;sub _:lvalue{$_}<<s;#';<<s#'''
+def printf(a,*b):import sys;sys.stdout.write(a%b),
+s
+#*/
+_=" #include/*%cs='''*/<stdio.h>%cmain(){char*_;/*==;sub _:lvalue{%c_}<<s;#';<<s#'''%cdef printf(a,*b):import sys;sys.stdout.write(a%%b),%cs%c#*/%c_=%c%s%c;printf(_,10,10,36,10,10,10,10,34,_,34,10,10,10,10);%c#/*%cs='''*/%c}//'''#==%c";printf(_,10,10,36,10,10,10,10,34,_,34,10,10,10,10);
+#/*
+s='''*/
+}//'''#==
